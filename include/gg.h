901107931523123505edb07abf2f11b380cd067a
Emiliano Firmino <emiliano.firmino@gmail.com>
2018-05-23 19:54:09 +0200

    Setup project build
    * Added CMakeLists.txt
    * Added rules to .gitignore
    * Hello Word .cpp and .h
    
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

---
+++ include/gg.h
@@ -0,0 +1,13 @@
+/*
+ * gg.h
+ * Copyright (C) 2018 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef GG_H
+#define GG_H
+
+#include <iostream>
+
+#endif /* !GG_H */
---
+++ src/gg.cpp
@@ -0,0 +1,14 @@
+/*
+ * gg.cpp
+ * Copyright (C) 2018 Emiliano Firmino <emiliano.firmino@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "gg.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!";
+    return 0;
+}
+
