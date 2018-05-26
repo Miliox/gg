b8ad8407e00c127bffc0a3a835975bb7a499d699
Emiliano Firmino <emiliano.firmino@gmail.com>
2018-05-26 22:43:57 +0200

    Integrate catch unit test library
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- src/gg.cpp
+++ src/gg.cpp
@@ -7,8 +7,24 @@
 
 #include "gg.h"
 
+#include <string>
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
 int main(int argc, char** argv) {
-    std::cout << "Hello, World!";
+    bool runTests = false;
+
+    for (int i = 0; i < argc; i++) {
+        if (std::string(argv[i]) == "--run-tests") {
+            runTests = true;
+        }
+    }
+
+    if (runTests) {
+        Catch::Session().run(1, argv);
+    }
+
     return 0;
 }
 
