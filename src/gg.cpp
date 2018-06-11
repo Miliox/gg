690b0b847f2a7575cd05641d45d8491f98afd406
Emiliano Firmino <emiliano.firmino@gmail.com>
2018-06-11 20:08:32 +0200

    Create window with SDL
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- src/gg.cpp
+++ src/gg.cpp
@@ -13,6 +13,7 @@
 
 #include <string>
 
+#include <SDL2/SDL.h>
 
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
@@ -30,6 +31,17 @@ int main(int argc, char** argv) {
         Catch::Session().run(1, argv);
     }
 
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+        std::cerr << "Fail to init SDL: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("GG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 288, 0);
+    if (window == NULL) {
+        std::cerr << "Fail to create window: " << SDL_GetError() << "\n";
+        return -1;
+    }
+
     CPU cpu;
     MMU mmu;
 
@@ -38,15 +50,28 @@ int main(int argc, char** argv) {
     cpu.read8  = [&](u16 addr) -> u8 { return mmu.read8(addr); };
     cpu.write8 = [&](u16 addr, u8 val) { mmu.write8(addr, val); };
 
+
     u64 ticks = 0;
-    for (;;) {
+    bool running = true;
+    while (running) {
         u8 elapsed = cpu.cycle();
         ticks += elapsed;
 
         mmu.step(elapsed);
-        syncer.step(elapsed);
+
+        if (syncer.step(elapsed)) {
+            // frame synced
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                switch (event.type) {
+                    case SDL_QUIT: running = false; break;
+                }
+            }
+        }
     }
 
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
 
